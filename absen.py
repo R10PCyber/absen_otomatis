6d48f600c427260fd13b22458ddb2d5eeabb0c51
R10PCyber <rijalpahlawan10@gmail.com>
2025-11-13 09:03:12 +0700

    Update absen.py

--- absen.py
+++ absen.py
@@ -1,4 +1,8 @@
 import datetime
+import os
+
+print("Username:", os.environ.get("ABSEN_USER"))
+print("Password:", os.environ.get("ABSEN_PASS"))
 
 # Contoh sederhana (belum login ke web)
 print("Script absen otomatis dijalankan pada:", datetime.datetime.now())
